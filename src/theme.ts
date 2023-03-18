7ad18b06e263b6a2bfce7f76f8aff20641beee50
yhwa <global user>
2023-03-18 13:04:47 +0900

    Project setting

---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "rgb(20,20,20)",
+  textColor: "rgb(229,229,229)",
+  accentColor: "rgb(84,185,197)"
+}
